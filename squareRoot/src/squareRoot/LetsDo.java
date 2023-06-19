99e2c9e86b5603c1df7976ab229cc9b9f398443c
admin <admin@DESKTOP-ODPVLGR>
2023-06-19 23:23:48 +0530

    New Project 2 added

---
+++ squareRoot/src/squareRoot/LetsDo.java
@@ -0,0 +1,10 @@
+package squareRoot;
+
+public class LetsDo {
+
+	public static void main(String[] args) {
+		System.out.println("Lets Do the Right Thing");
+
+	}
+
+}
---
+++ squareRoot/src/squareRoot/SquareRoot.java
@@ -0,0 +1,9 @@
+package squareRoot;
+import java.util.Scanner;
+public class SquareRoot {
+public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+	double  a= sc.nextDouble();
+	System.out.println(Math.sqrt(a));	
+}
+}
