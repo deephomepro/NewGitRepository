6efdc463866ef590365a82fb76c611943b80d38e
admin <admin@DESKTOP-ODPVLGR>
2023-06-20 17:42:37 +0530

    Added new Project oddEvenNumber

---
+++ oddEvenNumber/src/oddEvenNumber/OddEvenNumber.java
@@ -0,0 +1,16 @@
+package oddEvenNumber;
+import java.util.Scanner;
+public class OddEvenNumber {
+public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+	int x= sc.nextInt();
+	if(x % 2 == 0){
+	System.out.println("Even Number");
+	
+	} else { 
+	System.out.println("odd Number");
+	
+	
+}
+}
+}
